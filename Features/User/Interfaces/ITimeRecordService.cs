6fc53dbb1423102817776606d34c2645c9b51a09
CANMERTINYO <38213551+c4nzin@users.noreply.github.com>
2024-07-01 16:30:28 +0300

    feat: add mapping with TimeRecord

--- Features/User/Interfaces/ITimeRecordService.cs
+++ Features/User/Interfaces/ITimeRecordService.cs
@@ -4,7 +4,10 @@ namespace time_tracker_case;
 
 public interface ITimeRecordService
 {
-    Task<TimeRecord> CreateTimeRecord(CreateTimeRecordDto createTimeRecordDto, Guid projectId);
+    Task<CreateTimeRecordDto> CreateTimeRecord(
+        CreateTimeRecordDto createTimeRecordDto,
+        Guid projectId
+    );
     Task<List<TimeRecord>> GetTimeRecords(Guid projectId);
     Task<List<TimeRecord>> FilterTimeRecords(
         FilterTimeRecordDto filterTimeRecordDto,
